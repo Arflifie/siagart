78d129c5433a54519d31674c5380b6f54652eb5d
agtfie <fitriagstnaa@gmail.com>
2025-11-19 19:13:59 +0700

    admin

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,12 +18,10 @@
 </head>
 <body class="bg-gray-100 text-gray-700 font-[Montserrat]">
 
-    @include('layouts.navunregis')
-
     <div class="min-h-screen flex flex-col justify-center px-6">
         @yield('content')
     </div>
-
+    
     @include('layouts.footer')
 
 </body>
