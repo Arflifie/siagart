b6ceef7f10178c5c4798b4d0f4c7cefbee9cdcb0
Arfun Ali Yafie <alinews120@gmail.com>
2025-11-30 12:40:10 +0700

    maintenance1

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
+// --- TAMBAHKAN BAGIAN INI ---
+// Paksa PHP menampilkan error ke layar, memotong settingan Laravel
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// ----------------------------
+
 require __DIR__ . '/../public/index.php';
