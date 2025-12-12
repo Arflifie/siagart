9daeed3072db27b513e64381676c4d0f83b154cb
Arfun Ali Yafie <alinews120@gmail.com>
2025-12-12 18:51:26 +0700

    fix error number wa

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -125,7 +125,7 @@
                         </div>
                     </div>
                     @if($report->wa_number)
-                        <a href="https://wa.me/{{ $report->wa_number }}" target="_blank" class="w-full py-2.5 px-4 bg-green-50 text-green-700 hover:bg-green-100 font-medium rounded-lg flex justify-center gap-2 border border-green-200">
+                        <a href="https://wa.me/{{ $report->whatsapp_url }}" target="_blank" class="w-full py-2.5 px-4 bg-green-50 text-green-700 hover:bg-green-100 font-medium rounded-lg flex justify-center gap-2 border border-green-200">
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg> Hubungi via WhatsApp
                         </a>
                     @endif
